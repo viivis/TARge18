884c71719a420f26b0363c316b18c3e9bfa73003
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2019-05-23 17:16:51 +0300

    Loo singleRandom klass, millega lahendatakse Random arvude  tsüklis luues kordumise probleem.

---
+++ Kangelased/Kangelased/SingleRandom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kangelased
+{
+	class SingleRandom: Random
+	{
+		static SingleRandom _Instance;
+		public static SingleRandom Instance
+		{
+			get
+			{
+				if (_Instance==null)
+				{
+					_Instance = new SingleRandom();
+				}
+				return _Instance;
+			}
+		}
+		private SingleRandom() { }
+	}
+}
